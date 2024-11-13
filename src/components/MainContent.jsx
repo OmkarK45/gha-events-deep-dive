577b9932d337345d0777b075d44d4c4c4e0c067d
Omkar <omkarak45@gmail.com>
2024-11-13 23:23:46 +0530

    added comments [skip ci]

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import HelpArea from './HelpArea';
 
+// yet another comment 
+
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
 
